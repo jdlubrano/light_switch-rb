e3c8b690f811c5c91205699d04dd1fba29ab9c4e
Joel Lubrano <joel.lubrano@gmail.com>
2025-03-14 00:16:51 -0400

    Use rails runner for initializer tests

--- scripts/initializers_test.rb
+++ scripts/initializers_test.rb
@@ -1,15 +1,5 @@
-ENV["RAILS_ENV"] = "test"
+# frozen_string_literal: true
 
-require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
-require "rails/test_help"
-require "minitest/mock"
-
-module LightSwitch
-  class InitializerTest < ActiveSupport::TestCase
-    test "switches configured in an initializer get created" do
-      assert LightSwitch::Switch.exists?(name: :switch_defined_in_initializer, state: :on)
-    end
-  end
+unless LightSwitch::Switch.exists?(name: :switch_defined_in_initializer, state: :on)
+  raise "LightSwitch initializer failed to create switches"
 end
